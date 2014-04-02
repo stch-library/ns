4c5be74a64ee69484ae5bd0989a9e1c540fe2dc3
David Sargeant <david@dsargeant.com>
2014-04-02 10:06:38 -0400

    Upgraded to Clojure 1.6.0.

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
   :url "https://github.com/stch-library/ns"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [ns-tracker "0.2.2"]])
